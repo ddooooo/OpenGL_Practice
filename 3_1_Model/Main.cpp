4504d1b72b74baa27756dda227fa87963f498356
ddooooo <shb05377@gmail.com>
2022-08-17 23:47:53 +0900

    Load a Model with baked Texture

--- 3_1_Model/Main.cpp
+++ 3_1_Model/Main.cpp
@@ -27,7 +27,7 @@ int main(int argc, char** argv)
 
 	renderer->LoadVertices();
 
-	renderer->LoadModel("Models/Cube/Cube_Texture.obj", false);
+	renderer->LoadModel("Models/Link/Link.obj", false);
 
 	renderer->LoadShader();
 
